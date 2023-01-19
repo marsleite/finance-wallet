16c04e0ea4777827b935f746131b08c9d355e7b1
Marcelo <marcelo.leite@mercadolivre.com>
2023-01-19 00:10:11 -0300

    [feat] initial commit

---
+++ src/main/kotlin/com/alpha/financewallet/FinanceWalletApplication.kt
@@ -0,0 +1,11 @@
+package com.alpha.financewallet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceWalletApplication
+
+fun main(args: Array<String>) {
+    runApplication<FinanceWalletApplication>(*args)
+}
